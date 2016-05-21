ef4e5b7ed9ba3d13f27ef65d92572c52d6f3c189
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2016-05-22 00:57:17 +0300

    Works on Desktop

--- CMAKE_VERSION/src/main.cpp
+++ CMAKE_VERSION/src/main.cpp
@@ -1,10 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
+    printf("foo");
+    qDebug() << engine.importPathList();
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     qputenv("QML_IMPORT_TRACE", "1");
     return app.exec();
